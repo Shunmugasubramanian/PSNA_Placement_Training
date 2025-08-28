54abca8a6ad39515ecc087d3cbf1dbdbbba36ea0
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-28 22:16:03 +0530

    Create ArithmeticOperations.java

---
+++ Basics/ArithmeticOperations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+      int a = 20, b = 10;
+      int add = a + b;
+      int sub = a - b;
+      int mul = a * b;
+      int div = a / b;
+      int rem = a % b;
+      System.out.println(add+" "+sub+" "+mul+" "+div+" "+rem);
+    }
+}
+      
