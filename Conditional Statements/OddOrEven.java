eb18fd85e9987d196edf68962eef9955219f07d4
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-28 22:24:56 +0530

    Create OddOrEven.java

---
+++ Conditional Statements/OddOrEven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int number =  s.nextInt();
+        if(number % 2 == 0)
+          System.out.println("Even");
+        else
+          System.out.println("Odd");
+    }
+} 
