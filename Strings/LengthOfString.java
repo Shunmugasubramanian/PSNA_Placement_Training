9969d6bfccd05016242db4a8f621c927bacb95cb
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 16:04:58 +0530

    Create LengthOfString.java

---
+++ Strings/LengthOfString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int i,len = 0;
+        for(i = 0; i < str.length(); i++)
+            len++;
+       System.out.println(len);
+    }
+}
