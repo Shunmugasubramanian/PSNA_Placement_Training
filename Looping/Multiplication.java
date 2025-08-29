030193d1c6eeda22d7c859cc11c510c8744b84f0
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 14:56:20 +0530

    Create Multiplication.java

---
+++ Looping/Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {        
+        Scanner s = new Scanner(System.in);
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int i, sum = 0;
+        //Multiplication without '*' operator
+        for(i = 1; i <= n2; i++){
+                sum += n1;
+        }
+        System.out.println(sum);
+        s.close();
+    }
+}
