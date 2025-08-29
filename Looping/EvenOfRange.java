6bf7ba4b8cfbc26ec7537d2a657e95944b074c53
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 14:39:56 +0530

    Create EvenOfRange.java

---
+++ Looping/EvenOfRange.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {        
+        Scanner s = new Scanner(System.in);
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();      
+        for(int i = n1; i <= n2; i++) {
+            if(i % 2 == 0) 
+                System.out.print(i+" ");           
+        }
+    }
+}
