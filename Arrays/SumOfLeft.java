30d09e5a6d4d248a057deda840245cf61bd4e9af
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 15:52:38 +0530

    Create SumOfLeft.java

---
+++ Arrays/SumOfLeft.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] a = new int[n];
+        int i, sum = 0;
+        for(i = 0; i <n; i++) {
+            System.out.print(sum + " ");
+            a[i] = s.nextInt();
+            sum += a[i];
+        }
+    }
+}
