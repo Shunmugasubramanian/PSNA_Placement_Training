5c1fbf3875a00e00f40b7559b7c01aeebf07a3f7
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 16:12:37 +0530

    Create ReverseString.java

---
+++ Strings/ReverseString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        for(int i = str.length()-1; i >= 0; i--)
+            System.out.print(str.charAt(i));
+    }
+}
