080e06f338da5154985d663ff83e4d9f56a3d937
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 15:39:11 +0530

    Create HollowPattern.java

---
+++ Patterns/HollowPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i, j;        
+        for(i = 1; i <= n; i++) {
+            for(j = 1; j <= n; j++) {
+                if(i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
