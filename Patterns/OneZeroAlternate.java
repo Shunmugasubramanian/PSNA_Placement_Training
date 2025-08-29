4da78f8b4aaec7ce33e120ade5f7ba7b305f6dfa
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 15:34:51 +0530

    Create OneZeroAlternate.java

---
+++ Patterns/OneZeroAlternate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i, j;        
+        for(i = 1; i <= n; i++) {
+            for(j = 1; j <= n; j++) {
+                if((i + j) % 2 == 0)
+                   System.out.print("1");
+                else
+                   System.out.print("0"); 
+            }
+            System.out.println();
+        }   
+    }
+}
