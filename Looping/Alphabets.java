4df79e1e85ae756af00e8a289318b98e879e8554
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 14:57:57 +0530

    Create Alphabets.java

---
+++ Looping/Alphabets.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i = 1; i <= n; i++){
+             System.out.println("a b c d e f g h i j k l m n o p q r s t u v w x y z");
+        }
+    }
+}
