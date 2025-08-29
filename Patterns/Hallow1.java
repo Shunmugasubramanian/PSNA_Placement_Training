06e69cea1f1088f58c9a7ccf7e6bbcb521adff88
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 15:40:11 +0530

    Create Hallow1.java

---
+++ Patterns/Hallow1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int n1 = (2*n)-1;
+        int i, j;        
+        for(i = 1; i <= n1; i++) {
+            for(j = 1; j <= n1; j++) {
+                if(i == 1 || i == n1 || j == 1 || j == n1 || i == j || i + j == n1 + 1 )
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
