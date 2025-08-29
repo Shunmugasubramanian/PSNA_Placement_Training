3e8213549af39705a41510d41045580f5649f2df
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-29 14:44:19 +0530

    Create RangeOfN.java

---
+++ Looping/RangeOfN.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i;
+        for(i = 1; i <= n; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
