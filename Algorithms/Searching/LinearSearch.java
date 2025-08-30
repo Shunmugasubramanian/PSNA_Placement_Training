a65ecaca967c0c6f1648d27940ca96c4b28ca9ef
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-30 22:52:50 +0530

    Update LinearSearch.java

--- Algorithms/Searching/LinearSearch.java
+++ Algorithms/Searching/LinearSearch.java
@@ -1,1 +1,24 @@
-
+import java.util.*;
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        boolean flag = false;
+        int i;
+        int[] arr = new int[n];
+        for(i = 0; i < n; i++)
+            arr[i] = s.nextInt();
+        System.out.print("Enter the Search Element: ");
+        int target = s.nextInt();
+        for(i = 0; i < n; i++) {
+            if (arr[i] == target) {
+                flag = true;
+                break;
+            }
+        }
+        if(flag == true) 
+            System.out.println("Element found!");
+        else 
+            System.out.print("Element Not found");
+    }
+}
