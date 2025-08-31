d988cb03c770a6b81d0914dacbc38004d249077f
Shunmuga Subramanian <147188024+Shunmugasubramanian@users.noreply.github.com>
2025-08-31 20:52:23 +0530

    Create NthTermOfAP.java

---
+++ Digit Manipulation/NthTermOfAP.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+	    int a = 2, b = 3, n = 5;
+	    int nth = (a + (n - 1) * (b - a));
+	    System.out.println(nth); //o/p => 6 (2, 3, 4, 5, 6)
+	}
+}
